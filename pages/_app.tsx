bbf29434b4ad3fb5bcf245c15d0bac7c3dbb42a8
Juni Kim <junikimm717@gmail.com>
2022-07-13 10:23:41 -0400

    custom fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,24 @@
-import type { AppProps } from 'next/app';
-import Layout from '@/layouts/Layout';
-import { ChakraProvider, CSSReset } from '@chakra-ui/react';
-import Head from 'next/head';
+import type { AppProps } from "next/app";
+import Layout from "@/layouts/Layout";
+import { ChakraProvider, CSSReset } from "@chakra-ui/react";
+import Head from "next/head";
+import "../globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<ChakraProvider>
-			<Head>
-				<meta
-					name='description'
-					content='TeXbld is a build tool that uses a simple configuration syntax and the docker virtualization technology to ensure reproducible builds across different operating systems and environments.'
-				/>
-				<link rel='shortcut icon' href='/favicon.svg' />
-			</Head>
-			<Layout>
-				<CSSReset />
-				<Component {...pageProps} />
-			</Layout>
-		</ChakraProvider>
-	);
+  return (
+    <ChakraProvider>
+      <Head>
+        <meta
+          name="description"
+          content="TeXbld is a build tool that uses a simple configuration syntax and the docker virtualization technology to ensure reproducible builds across different operating systems and environments."
+        />
+        <link rel="shortcut icon" href="/favicon.svg" />
+      </Head>
+      <Layout>
+        <CSSReset />
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  );
 }
 export default MyApp;
