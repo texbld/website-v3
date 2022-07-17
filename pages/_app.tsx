718dbeb1e63ca1799170ee3e3aaa8db387aa0e75
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2022-07-17 18:06:03 -0400

    color mode changed to dark

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,29 @@
-import type { AppProps } from 'next/app';
-import Layout from '@/layouts/Layout';
-import { ChakraProvider, CSSReset } from '@chakra-ui/react';
-import Head from 'next/head';
+import type { AppProps } from "next/app";
+import Layout from "@/layouts/Layout";
+import { ChakraProvider, CSSReset, extendTheme } from "@chakra-ui/react";
+import Head from "next/head";
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: "dark",
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<ChakraProvider>
-			<Head>
-				<meta
-					name='description'
-					content='TeXbld is a build tool that uses a simple configuration syntax and the docker virtualization technology to ensure reproducible builds across different operating systems and environments.'
-				/>
-				<link rel='shortcut icon' href='/favicon.svg' />
-			</Head>
-			<Layout>
-				<CSSReset />
-				<Component {...pageProps} />
-			</Layout>
-		</ChakraProvider>
-	);
+  return (
+    <ChakraProvider theme={theme}>
+      <Head>
+        <meta
+          name="description"
+          content="TeXbld is a build tool that uses a simple configuration syntax and the docker virtualization technology to ensure reproducible builds across different operating systems and environments."
+        />
+        <link rel="shortcut icon" href="/favicon.svg" />
+      </Head>
+      <Layout>
+        <CSSReset />
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  );
 }
 export default MyApp;
